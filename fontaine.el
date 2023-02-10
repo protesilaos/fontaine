protesilaos/fontaine

commit fa0097248e21b5534aac863d6215be97f1112efb

Remove faulty height from bold face
I discovered this by troubleshooting the problem reported by Joe
Higton on the ef-themes mailing list:
<https://lists.sr.ht/~protesilaos/ef-themes/%3C87mta80vtk.fsf%40heartofgold.mail-host-address-is-not-set%3E>.

It is a mistake to set a :height attribute for bold, as it should not
anyhow affect the underlying text beside the :weight and :family.

fontaine.el
@@ -438,7 +438,6 @@ ARGS are its routines."
   'bold
   (or (plist-get properties :bold-family) 'unspecified)
   (or (plist-get properties :bold-weight) 'bold)
-  (or (plist-get properties :variable-pitch-height) 1.0)
   frame))
 
 (fontaine--apply-preset

README.org
@@ -614,7 +614,7 @@ Fontaine is meant to be a collective effort.  Every bit of help matters.
 + Contributions to the code or manual :: Christopher League, Eli
   Zaretskii, Florent Teissier, Terry F. Torrey.
 
-+ Ideas and user feedback :: Ted Reed.
++ Ideas and user feedback :: Joe Higton, Ted Reed.
 
 * GNU Free Documentation License
 :PROPERTIES:

commit d4c958feb47d7b7cc2f0d91252bb7827af546dae

Pass correct height when setting the 'bold' face
This fixes a regression introduced by commit fa00972.  We would get an
error when invoking 'fontaine-set-preset' with a prefix argument.

fontaine.el
@@ -482,6 +482,7 @@ ARGS are its routines."
   'bold
   (or (plist-get properties :bold-family) 'unspecified)
   (or (plist-get properties :bold-weight) 'bold)
+  'unspecified
   frame))
 
 (fontaine--apply-preset
@@ -702,7 +703,7 @@ face."
   "Set `bold' attributes, optionally for FRAME."
   (let ((weight (intern (completing-read "Select weight for `bold': "
                                          fontaine--font-weights nil t))))
-    (fontaine--set-face-attributes 'bold 'unspecified weight frame)))
+    (fontaine--set-face-attributes 'bold 'unspecified weight 'unspecified frame)))
 
 (defun fontaine--set-italic (&optional frame)
   "Set `italic' attributes, optionally for FRAME."