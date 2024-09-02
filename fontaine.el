18e39174cc453fd47b2b27d0f6b7a431c7be73cb
Protesilaos Stavrou <info@protesilaos.com>
2024-09-02 10:57:48 +0300

    Update to fontaine version 2.1.0

--- fontaine.el
+++ fontaine.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/fontaine
-;; Version: 2.0.0
+;; Version: 2.1.0
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This file is NOT part of GNU Emacs.
