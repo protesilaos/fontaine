ef300edf9b1ff88a56c92ef3e6661ed246e07573
Protesilaos Stavrou <info@protesilaos.com>
2025-01-08 08:37:34 +0200

    Update copyright years

--- fontaine.el
+++ fontaine.el
@@ -1,6 +1,6 @@
 ;;; fontaine.el --- Set font configurations using presets -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2024  Free Software Foundation, Inc.
+;; Copyright (C) 2022-2025  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
