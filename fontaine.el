34d8b04bef350b270f52dc4defc9bac3e9617bf0
Protesilaos Stavrou <info@protesilaos.com>
2025-04-15 18:17:10 +0300

    Bump version to include commit 599dd46 in GNU ELPA build

--- fontaine.el
+++ fontaine.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/fontaine
-;; Version: 3.0.0
+;; Version: 3.0.1
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This file is NOT part of GNU Emacs.
